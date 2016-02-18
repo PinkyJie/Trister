4b71e4f34ec68006ee4c198eb5bfadc8f5461b3a
PinkyJie <pilixiaoxuanfeng@gmail.com>
2016-02-18 23:20:15 +0800

    working demo

---
+++ twitter_oauth.py
@@ -0,0 +1,173 @@
+import time
+import random
+import urllib
+import urllib2
+import cookielib
+import hmac
+import hashlib
+import re
+import config
+import logging
+
+logging.basicConfig()
+
+# constants
+CONSUMER_KEY = config.CONSUMER_KEY
+CONSUMER_SECRET = config.CONSUMER_SECRET
+REQUEST_TOKEN_URL = 'https://api.twitter.com/oauth/request_token'
+AUTHENTICATE_URL = 'https://api.twitter.com/oauth/authenticate'
+AUTHORIZE_URL = 'https://api.twitter.com/oauth/authorize'
+ACCESS_TOKEN_URL = 'https://api.twitter.com/oauth/access_token'
+CALLBACK_URL = None
+
+
+# utility functions
+def qt(s):
+    return urllib.quote(s, '~')
+
+
+def str2dict(s):
+    dic = {}
+    for param in s.split('&'):
+        (key, value) = param.split('=')
+        dic[key] = value
+    return dic
+
+
+def dict2str(dic):
+    return ', '.join(['%s="%s"' % (key, value) for key, value in dic.iteritems()])
+
+
+class TwitterOauthError(Exception):
+    def __init__(self, message, reason):
+        Exception.__init__(self, message)
+        self.reason = reason
+
+
+class TwitterOauth(object):
+    """docstring for TwitterOauth"""
+    def __init__(self, username, password):
+        super(TwitterOauth, self).__init__()
+        myCookie = urllib2.HTTPCookieProcessor(cookielib.CookieJar());
+        self.opener = urllib2.build_opener(myCookie)
+        self.username = username
+        self.password = password
+        self.logger = logging.getLogger(__name__)
+
+    def build_header(self, params, base_url, oauth_token_secret='', method='POST'):
+        common_params = {
+            'oauth_consumer_key': CONSUMER_KEY,
+            'oauth_version': '1.0',
+            'oauth_signature_method': 'HMAC-SHA1',
+            'oauth_timestamp': int(time.time()),
+            'oauth_nonce': ('%x' % random.getrandbits(64))
+        }
+        params.update(common_params)
+        if CALLBACK_URL:
+            params['oauth_callback'] = qt(CALLBACK_URL)
+        keys = sorted(list(params.keys()))
+        encoded = qt('&'.join(['%s=%s' % (key, params[key]) for key in keys]))
+        base_string = '%s&%s&%s' % (method, qt(base_url), encoded)
+        # print base_string
+        signing_key = CONSUMER_SECRET + '&' + oauth_token_secret
+        params['oauth_signature'] = qt(hmac.new(signing_key, base_string, hashlib.sha1).digest().encode('base64')[:-1])
+        return params
+
+    def get_request_token(self):
+        req = urllib2.Request(REQUEST_TOKEN_URL, headers={'Authorization': 'OAuth ' +
+                              dict2str(self.build_header({}, REQUEST_TOKEN_URL))})
+        res = None
+        try:
+            res = self.opener.open(req, {}).read()
+        except Exception, e:
+            self.logger.error('get request token error: ' + e.message)
+            raise TwitterOauthError(e.message,
+                                    'Failed to get access token. There maybe a server error. Try again later!')
+        else:
+            res_dict = str2dict(res)
+            if res_dict['oauth_callback_confirmed'] == 'true':
+                self.oauth_token = res_dict['oauth_token']
+                self.oauth_token_secret = res_dict['oauth_token_secret']
+                self.logger.debug('get request token: ok ')
+                self.logger.debug(res_dict)
+            else:
+                self.logger.error('get request token error: callback not true')
+                raise TwitterOauthError('', 'Failed to get access token. There maybe a server error. Try again later!')
+
+    def get_pin_code(self):
+        redirect_url = AUTHENTICATE_URL + '?oauth_token=' + self.oauth_token
+        res_temp = self.opener.open(redirect_url).read()
+        re_token = re.compile(r'<input name="authenticity_token" type="hidden" value="(\w+)"')
+        pin_params = {}
+        pin_params['authenticity_token'] = re_token.findall(res_temp)[0]
+        pin_params['oauth_token'] = self.oauth_token
+        pin_params['session[username_or_email]'] = self.username
+        pin_params['session[password]'] = self.password
+        pin_params['redirect_after_login'] = redirect_url
+        req = urllib2.Request(AUTHENTICATE_URL, urllib.urlencode(pin_params))
+        res = None
+        try:
+            res = self.opener.open(req)
+        except Exception, e:
+            self.logger.error('get pin code error: ' + e.message)
+            raise TwitterOauthError(e.message,
+                                    'Failed to get pin code. There maybe a server error. Try again later!')
+        else:
+            re_pin = re.compile(r'<code>(\d*)</code>')
+            pin_code = re_pin.findall(res.read())
+            if len(pin_code) > 0:
+                self.pin_code = pin_code[0]
+                self.logger.debug('get pin code ok: ' + self.pin_code)
+            else:
+                self.logger.error('get pin code error: username or password error')
+                raise TwitterOauthError('', 'Failed to get pin code. Check your Username or Password!')
+
+    def get_access_token(self):
+        access_params = {}
+        access_params['oauth_token'] = self.oauth_token
+        access_params['oauth_verifier'] = self.pin_code
+        req = urllib2.Request(ACCESS_TOKEN_URL, headers={'Authorization': 'OAuth ' +
+                              dict2str(self.build_header(access_params, ACCESS_TOKEN_URL, self.oauth_token_secret))})
+        res = None
+        try:
+            res = self.opener.open(req, {}).read()
+        except Exception, e:
+            self.logger.error('get access token error: ' + e.message)
+            raise TwitterOauthError(e.message,
+                                    'Failed to access token. There maybe a server error. Try again later!')
+        else:
+            res_dict = str2dict(res)
+            self.access_token = res_dict['oauth_token']
+            self.access_token_secret = res_dict['oauth_token_secret']
+            self.screen_name = res_dict['screen_name']
+            self.logger.debug('get access token: ok ')
+            self.logger.debug(res_dict)
+
+    def oauth(self):
+        try:
+            self.get_request_token()
+        except TwitterOauthError, e1:
+            raise e1
+        else:
+            try:
+                self.get_pin_code()
+            except TwitterOauthError, e2:
+                raise e2
+            else:
+                try:
+                    self.get_access_token()
+                except TwitterOauthError, e3:
+                    raise e3
+
+
+if __name__ == '__main__':
+    # test
+    t = TwitterOauth('xxx', 'xxx')
+    try:
+        t.oauth()
+    except TwitterOauthError, e:
+        print e.reason
+    else:
+        print t.access_token
+        print t.access_token_secret
+        print t.screen_name
---
+++ wsgi.py
@@ -0,0 +1,260 @@
+# coding=utf-8
+
+import os
+from functools import wraps
+import json
+import time
+
+from tweepy import (parsers, OAuthHandler, API, TweepError)
+from flask import (Flask, request, session, g)
+
+from twitter_oauth import (TwitterOauth, TwitterOauthError)
+
+application = app = Flask(__name__, static_folder='static', static_url_path='')
+app.config.from_object('config')
+
+
+def jsonify(f):
+    @wraps(f)
+    def _wrapped(*args, **kwargs):
+        from flask import jsonify as flask_jsonify
+        try:
+            result_dict = f(*args, **kwargs)
+        except Exception as e:
+            result_dict = dict(success=False)
+            result_dict['content'] = 'Operation failed: ' + e.message
+            if app.config['DEBUG']:
+                from traceback import format_exc
+                result_dict['exc_info'] = format_exc(e)
+        return flask_jsonify(**result_dict)
+    return _wrapped
+
+
+def rebuild_api():
+    twit = OAuthHandler(app.config['CONSUMER_KEY'], app.config['CONSUMER_SECRET'])
+    twit.set_access_token(session.get('trister_access_key'), session.get('trister_access_secret'))
+    return API(auth_handler=twit, parser=parsers.RawParser())
+
+
+def str2timestamp(time_str):
+    time_struct = time.strptime(time_str, '%a %b %d %H:%M:%S +0000 %Y')
+    return int(time.mktime(time_struct))
+
+
+def is_dm_with_user(dm, user, me):
+    sender = dm['sender']['screen_name']
+    receiver = dm['recipient']['screen_name']
+    return (sender == me and receiver == user) or (sender == user and receiver == me)
+
+
+@app.before_request
+def before_request():
+    # print request.endpoint
+    except_endpoints = ('root', 'static', 'is_login', 'oauth_login')
+    if request.endpoint not in except_endpoints:
+        if session.get('trister_access_key') and session.get('trister_access_secret'):
+            g.twit_api = rebuild_api()
+        else:
+            return app.send_static_file('index.html')
+
+
+@app.route('/', methods=['GET'])
+def root():
+    return app.send_static_file('index.html')
+
+
+@app.route('/is_login', methods=['GET'])
+@jsonify
+def is_login():
+    if session.get('trister_access_key') and session.get('trister_access_secret'):
+        return dict(success=True, content=1,
+                    user=dict(key=session.get('trister_access_key'),
+                              secret=session.get('trister_access_secret'),
+                              name=session.get('trister_user_name')
+                              )
+                    )
+    else:
+        return dict(success=True, content=0)
+
+
+@app.route('/login', methods=['POST'])
+@jsonify
+def oauth_login():
+    t = TwitterOauth(request.form['name'], request.form['password'])
+    try:
+        t.oauth()
+    except TwitterOauthError, e:
+        return dict(success=False, content=e.reason)
+    else:
+        session.permanent = True
+        session['trister_access_key'] = t.access_token
+        session['trister_access_secret'] = t.access_token_secret
+        session['trister_user_name'] = t.screen_name
+        return dict(success=True, content=dict(key=t.access_token, secret=t.access_token_secret, name=t.screen_name))
+
+
+@app.route('/home', methods=['GET'])
+def get_home():
+    page_arg = int(request.args['page'])
+    count_arg = int(request.args['count'])
+    tweets = g.twit_api.home_timeline(page=page_arg, count=count_arg)
+    return tweets
+
+
+@app.route('/mention', methods=['GET'])
+def get_reply():
+    page_arg = int(request.args['page'])
+    count_arg = int(request.args['count'])
+    replys = g.twit_api.mentions_timeline(page=page_arg, count=count_arg)
+    return replys
+
+
+@app.route('/update', methods=['POST'])
+@jsonify
+def update_status():
+    try:
+        if request.form['type'] == 'Reply':
+            g.twit_api.update_status(status=request.form['tweet'], in_reply_to_status_id=request.form['tweet_id'])
+        else:
+            g.twit_api.update_status(request.form['tweet'])
+    except TweepError, e:
+        return dict(success=False, content='Failed to update tweet!', reason=json.loads(e.message))
+    else:
+        return dict(success=True, content='')
+
+
+@app.route('/favorite/<action>', methods=['POST'])
+@jsonify
+def favorite(action):
+    fail_msg = ''
+    try:
+        if action == 'add':
+            g.twit_api.create_favorite(request.form['tweet_id'])
+            fail_msg = 'Failed to create favorite tweet'
+        elif action == 'del':
+            g.twit_api.destroy_favorite(request.form['tweet_id'])
+            fail_msg = 'Failed to destory favorite tweet'
+    except TweepError, e:
+        return dict(success=False, content=fail_msg, reason=json.loads(e.message))
+    else:
+        return dict(success=True, content='')
+
+
+@app.route('/retweet', methods=['POST'])
+@jsonify
+def retweet_tweet():
+    try:
+        g.twit_api.retweet(request.form['tweet_id'])
+    except TweepError, e:
+        return dict(success=False, content='Failed to retweet tweet!', reason=json.loads(e.message))
+    else:
+        return dict(success=True, content='Retweet successfully!')
+
+
+@app.route('/destory/<entity>', methods=['POST'])
+@jsonify
+def destory_entity(entity):
+    try:
+        if entity == 'tweet':
+            g.twit_api.destroy_status(request.form['tweet_id'])
+            success_msg = 'Delete tweet successfully!'
+            fail_msg = 'Failed to delete tweet!'
+    except TweepError, e:
+        return dict(success=False, content=fail_msg, reason=json.loads(e.message))
+    else:
+        return dict(success=True, content=success_msg)
+
+
+@app.route('/dm', methods=['GET'])
+def get_direct_message():
+    page_arg = int(request.args['page'])
+    count_arg = int(request.args['count'])
+    received_dms = json.loads(g.twit_api.direct_messages(page=page_arg, count=count_arg))
+    sent_dms = json.loads(g.twit_api.sent_direct_messages(page=page_arg, count=count_arg))
+    dms = received_dms + sent_dms
+    dm_list = []
+    me = session.get('trister_user_name')
+    senders = [dm['sender']['screen_name'] for dm in dms]
+    recipients = [dm['recipient']['screen_name'] for dm in dms]
+    all_users = set(senders + recipients)
+    users_expcept_me = [user for user in all_users if user != me]
+    for user in users_expcept_me:
+        _dict = {}
+        _dict['dms'] = [dm for dm in dms if is_dm_with_user(dm, user, me)]
+        dm_list.append(_dict)
+    json_str = json.dumps(dm_list)
+    return json_str
+
+
+@app.route('/dm/create', methods=['POST'])
+@jsonify
+def create_new_dm():
+    try:
+        dm = g.twit_api.send_direct_message(
+            screen_name=request.form['screen_name'],
+            text=request.form['text']
+        )
+    except TweepError, e:
+        return dict(success=False, content='Failed to send DM!', reason=json.loads(e.message))
+    else:
+        return dict(success=True, content=json.loads(dm))
+
+
+@app.route('/threads/<tweet_id>', methods=['GET'])
+@jsonify
+def get_reply_threads(tweet_id):
+    try:
+        threads = []
+        while tweet_id:
+            tweet = json.loads(g.twit_api.get_status(tweet_id))
+            threads.append(tweet)
+            tweet_id = tweet['in_reply_to_status_id_str']
+    except TweepError, e:
+        return dict(success=False, content='Failed to send DM!', reason=json.loads(e.message))
+    else:
+        return dict(success=True, content=threads)
+
+
+@app.route('/user/<screen_name>', methods=['GET'])
+@jsonify
+def get_user(screen_name):
+    try:
+        user = g.twit_api.get_user(screen_name=screen_name)
+    except TweepError, e:
+        return dict(success=False, content='Failed to find @' + screen_name, reason=json.loads(e.message))
+    else:
+        return dict(success=True, content=json.loads(user))
+
+
+@app.route('/lists/<screen_name>', methods=['GET'])
+def get_all_lists(screen_name):
+    if screen_name == '***':
+        screen_name = None
+    lists_sub = json.loads(g.twit_api.lists_all(screen_name=screen_name))
+    lists_added = []
+    cursor = -1
+    while cursor != 0:
+        _dict = json.loads(g.twit_api.lists_memberships(screen_name=screen_name, cursor=cursor))
+        lists_added += _dict['lists']
+        cursor = _dict['next_cursor']
+    for l in lists_sub:
+        l['type'] = 'Subscribe'
+    for l in lists_added:
+        l['type'] = 'Membership'
+    lists = lists_sub + lists_added
+    lists_json = json.dumps(lists)
+    return lists_json
+
+
+@app.route('/list/timeline/<list_id>', methods=['GET'])
+def get_list_timeline(list_id):
+    page_arg = int(request.args['page'])
+    count_arg = int(request.args['count'])
+    tweets = g.twit_api.list_timeline(list_id=list_id, page=page_arg, count=count_arg)
+    return tweets
+
+
+app.secret_key = '\xfcM\xf7\xd4\x03\x14\x1e<\xe1\xd4Sn\xed\xa5e\x96\xb7\x8aq\x82\xed\x10\xdc\x93'
+if __name__ == "__main__":
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=app.config['DEBUG'])
